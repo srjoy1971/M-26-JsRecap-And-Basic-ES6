8b14dae4f87be50cbb16d6cd25d23953fb8d1836
srjoy1971 <srjoy406@gmail.com>
2026-03-01 17:46:22 +0600

    add a file let-const.consepts is let is changeable , const not changeable

---
+++ let-const.js
@@ -0,0 +1,25 @@
+
+const name = 'Bangladesh';
+// name = 'Singapore';
+const countryName = 'BD_' + name;
+console.log(countryName);
+
+let price = 120;
+price = 150;
+console.log(price);
+
+// var name22 = 'Bangladesh';
+
+const dress = ['Shirt', 'Pant', 'Shoe'];
+// dress = ['Shirt', 'Pant', 'Shoe', 'Belt'];
+dress.push('Belt');
+console.log(dress);
+
+const student = {
+    name: 'John',
+    age: 25
+}
+
+student.name = 'Jane';
+student.age = 30;
+console.log(student);
